7c10fdfcb56df5b3f376d015c294093ec50c6b20
Giane <matteo.gianello@creactives.com>
2016-11-17 12:35:43 +0100

    primo commit

---
+++ src/main/java/com/giane/app/App.java
@@ -0,0 +1,16 @@
+package com.giane.app;
+
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static void main(String[] args) {
+    JDialog applet = new JDialog();
+    applet.setSize(200,200);
+    //applet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    applet.setVisible(true);
+  }
+}
